13f82b1d709b4fcae296cb9c79cdba93d9f5087f
Mark Arranz <mark.arranz@gmail.com>
2023-07-26 20:14:54 +0000

    Initial commit.

---
+++ GrpcGreeter/Program.cs
@@ -0,0 +1,36 @@
+using GrpcGreeter.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+// Register JSON Transcoding 
+builder.Services.AddGrpc().AddJsonTranscoding();
+// Configure Swashbuckle to include gRPC endpoints.
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo { Title = "gRPC transcoding", Version = "v1" });
+
+    var filePath = Path.Combine(System.AppContext.BaseDirectory, "GrpcGreeter.xml");
+    c.IncludeXmlComments(filePath);
+    c.IncludeGrpcXmlComments(filePath, includeControllerXmlComments: true);
+});
+
+var app = builder.Build();
+
+// Enable Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+});
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
